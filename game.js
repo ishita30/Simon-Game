9e30d57095f119c995b267759ee87303860d7ce6
ishita30 <ishitasinghal30@gmail.com>
2021-08-16 15:36:00 +0530

    Update game.js

--- game.js
+++ game.js
@@ -2,4 +2,6 @@ var buttonColours=["red", "blue", "green", "yellow"];
 function nextSequence()
 {
   var randomNumber= Math.floor(Math.random() * 3 + 1);
+  var randomChosenColour=buttonColours[randomNumber];
+
 }
