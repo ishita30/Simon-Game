804263a589339372c2cbb916cf313d8675a8d9f9
ishita30 <ishitasinghal30@gmail.com>
2021-08-17 11:10:22 +0530

    Update game.js

--- game.js
+++ game.js
@@ -5,5 +5,7 @@ function nextSequence()
   var randomNumber= Math.floor(Math.random() * 3 + 1);
   var randomChosenColour=buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);//to animate flash
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
