7036c60adb2f28b0b7d1d3ed34b874d157ee1cc0
ishita30 <ishitasinghal30@gmail.com>
2021-08-21 06:35:22 +0530

    Update game.js

--- game.js
+++ game.js
@@ -7,15 +7,21 @@ function nextSequence()
   var randomChosenColour=buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);//to animate flash
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
+  playSound(randomChosenColour);
+
 }
 $(".btn").click(
 function()
 {
   var userChosenColour= $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
 
 
 }
 );
+function playSound(name)
+{
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
